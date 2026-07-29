a885c1df90f4b5af36b4fa45093846df61c3bc51
Jacob Matthews <jacobmatthews@potomacdb.onmicrosoft.com>
2026-07-29 10:07:07 -0400

    fix: bypass stale favicon cache

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export const metadata: Metadata = {
     description: siteConfig.description,
     icons: {
         icon: [
-            { url: "/favicon.ico", sizes: "any" },
+            { url: "/cabeus-favicon.ico", sizes: "any" },
             { url: "/icon-192.png", type: "image/png", sizes: "192x192" },
             { url: "/icon-512.png", type: "image/png", sizes: "512x512" },
         ],
